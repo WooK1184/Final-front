ff898c8dbdcd2f9c73df16a7b461bb462bad85c5
WooK1184 <qkrtjddnr03@gmail.com>
2023-03-15 14:33:32 +0900

    db 경로 수정

--- BE/index.js
+++ BE/index.js
@@ -21,14 +21,14 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("http://localhost:3333/api/get", (req, res)=>{
+app.get("/api/get", (req, res)=>{
     const sqlQuery = "SELECT * FROM simpleboard;";
     db.query(sqlQuery, (err, result)=>{
         res.send(result);
     })
 })
 
-app.post("http://localhost:3333/api/insert", (req, res) => {
+app.post("/api/insert", (req, res) => {
     const title = req.body.title;
     const content = req.body.content;
     const sqlQuery = "INSERT INTO simpleboard (title, content) VALUES (?,?)";
